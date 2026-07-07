8999c4f36699e78f349460c34a38d58cda85b119
skinerbold <psamuelmendes@gmail.com>
2026-07-07 19:55:30 -0300

    cards tag blog

--- src/modules/blog/components/BlogCard.tsx
+++ src/modules/blog/components/BlogCard.tsx
@@ -16,19 +16,20 @@ export function BlogCard({ post }: BlogCardProps) {
     <article className="blog-card" style={style}>
       <Link className="blog-card__media" to={`/blog/${post.slug}`}>
         <img alt={post.coverImage.alt ?? post.title} src={post.coverImage.url} />
-        <span className="blog-card__category">{post.category.name}</span>
       </Link>
 
       <div className="blog-card__body">
-        <div className="blog-card__meta">
-          <span>{formatBlogDate(getPostDisplayDate(post))}</span>
-          <ReadingTime minutes={post.readingTimeMinutes} />
-        </div>
+        <span className="blog-card__category">{post.category.name}</span>
 
         <h2>
           <Link to={`/blog/${post.slug}`}>{post.title}</Link>
         </h2>
         <p>{post.summary}</p>
+
+        <div className="blog-card__meta">
+          <span>{formatBlogDate(getPostDisplayDate(post))}</span>
+          <ReadingTime minutes={post.readingTimeMinutes} />
+        </div>
 
         <Link className="blog-card__link" to={`/blog/${post.slug}`}>
           <span>Ler mais</span>
