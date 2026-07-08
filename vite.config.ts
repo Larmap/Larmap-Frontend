23757e5dd9c89dec32e79d443ab019d9e99c077a
skinerbold <psamuelmendes@gmail.com>
2026-07-08 19:43:20 -0300

    sitemap .xml add

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://larmap.com.br',
+      dynamicRoutes: [
+        '/',
+        '/aluguel',
+        '/compra',
+        '/novidades',
+        '/blog',
+        '/mapa',
+        '/sobre',
+        '/politica-de-privacidade',
+        '/politica-de-cookies',
+        '/termos-de-uso',
+        '/seja-parceiro',
+      ],
+    }),
+  ],
+
   server: {
     proxy: {
       '/api': {
